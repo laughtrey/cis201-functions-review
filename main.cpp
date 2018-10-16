e00cadce83aa65056299a9aefbc928d97bac74d2
Raymond <raymonl4963@student.vvc.edu>
2018-10-16 11:43:45 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
-
-
+#include<string>
 using namespace std;
 
+int is_a(string a);
 
 int main()
 {
-    cout << "Hello World"<< endl;
+  string a = "The quick brown fox jumped over the lazy dog";
+  cout << "The number of As is: " << is_a(a) << endl; 
+  return 0;
+}
 
-    return 0;
+int is_a(string a)
+{
+  int count = 0;
+   for (int i = 0; i < a.length(); i++)
+   {
+      char character = a.at(i);
+      if (character == 'a' || character == 'A')
+      {
+        count++;
+      }
+   }
+   return count;
 }
